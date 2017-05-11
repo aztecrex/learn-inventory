784598e20894e025df09501eeaba600c387a7129
Greg Wiley <gwiley@cj.com>
2017-05-11 14:08:15 -0700

    note that any specified instance data comes from db

--- src/main/java/com/cjpowered/learn/inventory/Item.java
+++ src/main/java/com/cjpowered/learn/inventory/Item.java
@@ -1,5 +1,10 @@
 package com.cjpowered.learn.inventory;
 
+/**
+ * An item we can stock.  Any instance data will be provided by
+ * by the database when you invoke {@link InventoryDatabase#stockItems()}
+ *
+ */
 public interface Item {
 
 }
