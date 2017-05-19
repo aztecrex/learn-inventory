ebed5bbfe4723ebce13aa8aaead224dc929ebe5c
Greg Wiley <aztec.rex@jammm.com>
2017-05-19 11:56:15 -0700

    on sale level clalculator

---
+++ src/test/java/test/com/cjpowered/learn/inventory/SaleStockCalculatorTest.java
@@ -0,0 +1,59 @@
+package test.com.cjpowered.learn.inventory;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.time.LocalDate;
+
+import org.junit.Test;
+
+import com.cjpowered.learn.inventory.InventoryDatabase;
+import com.cjpowered.learn.inventory.Item;
+import com.cjpowered.learn.inventory.SaleStockCalculator;
+import com.cjpowered.learn.inventory.StockCalculator;
+import com.cjpowered.learn.inventory.StandardStockCalculator;
+import com.cjpowered.learn.marketing.MarketingInfo;
+
+public class SaleStockCalculatorTest {
+
+    @Test
+    public void increasesRequiredDuringSale() {
+
+        // given
+        InventoryDatabase db = mock(InventoryDatabase.class);
+        MarketingInfo minfo = mock(MarketingInfo.class);
+                LocalDate today = LocalDate.ofEpochDay(900039);
+        final Item item = mock(Item.class);
+        when(minfo.onSale(item, today)).thenReturn(true);
+
+        final int normalLevel = 175;
+        final StockCalculator calc = new SaleStockCalculator();
+
+        // when
+        final int actual = calc.requiredStock(item, normalLevel, db, minfo, today);
+
+        assertEquals(normalLevel + 20, actual);
+        
+    }
+
+    @Test
+    public void noIncreaseNormally() {
+
+        // given
+        InventoryDatabase db = mock(InventoryDatabase.class);
+        MarketingInfo minfo = mock(MarketingInfo.class);
+                LocalDate today = LocalDate.ofEpochDay(900039);
+        final Item item = mock(Item.class);
+        when(minfo.onSale(item, today)).thenReturn(false);
+
+        final int normalLevel = 175;
+        final StockCalculator calc = new SaleStockCalculator();
+
+        // when
+        final int actual = calc.requiredStock(item, normalLevel, db, minfo, today);
+
+        assertEquals(normalLevel, actual);
+        
+    }
+
+}
